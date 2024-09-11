9fc69ae7d928ce14044700c1579ffa6f4557d32e
fabiosimplis <fabio.simplis@gmail.com>
2024-09-10 21:27:33 -0300

    ADD supertest

--- src/test/routes/editorasRoutes.test.js
+++ src/test/routes/editorasRoutes.test.js
@@ -1,4 +1,6 @@
-import { afterEach, beforeEach } from '@jest/globals';
+import request from 'supertest';
+// import wtfnode from 'wtfnode'; // pega o dump das requisições
+import { describe } from '@jest/globals';
 import app from '../../app.js';
 
 // iniciando o servidor
@@ -9,5 +11,48 @@ beforeEach(() => {
 });
 // finalizando o servidor
 afterEach(() => {
-  server.close();
+  server.close(() => {
+    // wtfnode.dump();
+  });
+});
+
+describe('GET em /editoras', () => {
+  it('Deve retornar uma lista de editoras', async () => {
+    const resposta = await request(app)
+      .get('/editoras')
+      .set('Accept', 'application/json')
+      .expect('content-type', /json/)
+      .expect(200);
+
+    expect(resposta.body[0].email).toEqual('e@e.com');
+  });
+});
+let idResposta;
+
+describe('POST em /editoras', () => {
+  it('Deve adicionar uma nova editora', async () => {
+    const resposta = await request(app)
+      .post('/editoras')
+      .send({
+        nome: 'CDC',
+        cidade: 'Sao Paulo',
+        email: 's@s.com',
+      })
+      .expect(201);
+    idResposta = resposta.body.content.id;
+  });
+});
+
+describe('GET /editores by id', () => {
+  it('Get o recurso adicionado', async () => {
+    await request(app)
+      .get(`/editoras/${idResposta}`);
+  });
+});
+
+describe('DELETE em /editores', () => {
+  it('Deletar o recurso adicionado', async () => {
+    await request(app)
+      .delete(`/editoras/${idResposta}`);
+  });
 });
